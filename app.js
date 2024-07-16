a1a1f98f6c4d0854ebdcc819e2bb04bbb413643e
artur789654 <artur789654@gmail.com>
2024-07-16 20:46:53 +0300

    added section featuredJobs with slider and fixed issues

--- app.js
+++ app.js
@@ -13,3 +13,61 @@ closeIcon.addEventListener('click', () => {
   menuIcon.style.display = 'block'; // Show hamburger
   closeIcon.style.display = 'none'; // Hide close icon
 });
+
+
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+const showAllButton = document.querySelector('.show-all');
+const cards = document.querySelector('.cardsJob');
+let offset = 0;
+const card = cards.children[0];
+const cardwidth = card.offsetWidth;
+
+const gap = parseFloat(getComputedStyle(cards).gap);
+const cardWidth = cardwidth + gap; 
+
+const updateTransform = () => {
+  cards.style.transform = `translateX(${offset}px)`;
+  updateArrowState();
+};
+
+const updateArrowState = () => {
+  if (cards.classList.contains('wrap')) {
+    leftArrow.removeEventListener('click', handleLeftArrowClick);
+    rightArrow.removeEventListener('click', handleRightArrowClick);
+  } else {
+    if (!leftArrow.classList.contains('disabled')) {
+      leftArrow.addEventListener('click', handleLeftArrowClick);
+    }
+    if (!rightArrow.classList.contains('disabled')) {
+      rightArrow.addEventListener('click', handleRightArrowClick);
+    }
+  }
+};
+
+const handleRightArrowClick = () => {
+  offset -= cardWidth;
+  if (offset <= -((cards.children.length) * cardWidth)) {
+    offset = 0; 
+  }
+  updateTransform();
+};
+
+
+const handleLeftArrowClick = () => {
+  offset += cardWidth;
+  if (offset > 0) {
+    offset = -((cards.children.length - 1) * cardWidth);
+  }
+  updateTransform();
+};
+
+showAllButton.addEventListener('click', () => {
+  cards.classList.toggle('wrap'); 
+  leftArrow.classList.toggle('disabled');
+  rightArrow.classList.toggle('disabled');
+  offset = 0; 
+  updateTransform();
+});
+
+updateArrowState();
