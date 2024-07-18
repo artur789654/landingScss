bf04495832d01782d3f269ae72dd8c8ddf47b87c
artur789654 <artur789654@gmail.com>
2024-07-18 14:14:12 +0300

    changeg class names to BEM methodology and replaced most of the svg in the img

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const menuIcon = document.getElementById('menuIcon');
-const closeIcon = document.getElementById('closeIcon');
-const nav = document.querySelector('.nav');
+const menuIcon = document.getElementById("menuIcon");
+const closeIcon = document.getElementById("closeIcon");
+const nav = document.querySelector(".nav");
 
-menuIcon.addEventListener('click', () => {
-  nav.classList.add('active'); // Add active class to show nav
-  menuIcon.style.display = 'none'; // Hide hamburger
-  closeIcon.style.display = 'block'; // Show close icon
+menuIcon.addEventListener("click", () => {
+  nav.classList.add("active"); // Add active class to show nav
+  menuIcon.style.display = "none"; // Hide hamburger
+  closeIcon.style.display = "block"; // Show close icon
 });
 
-closeIcon.addEventListener('click', () => {
-  nav.classList.remove('active'); // Remove active class to hide nav
-  menuIcon.style.display = 'block'; // Show hamburger
-  closeIcon.style.display = 'none'; // Hide close icon
+closeIcon.addEventListener("click", () => {
+  nav.classList.remove("active"); // Remove active class to hide nav
+  menuIcon.style.display = "block"; // Show hamburger
+  closeIcon.style.display = "none"; // Hide close icon
 });
 
-
-const leftArrow = document.querySelector('.left-arrow');
-const rightArrow = document.querySelector('.right-arrow');
-const showAllButton = document.querySelector('.show-all');
-const cards = document.querySelector('.cardsJob');
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+const showAllButton = document.querySelector(".show-all");
+const cards = document.querySelector(".cards-job");
 let offset = 0;
 const card = cards.children[0];
 const cardwidth = card.offsetWidth;
 
 const gap = parseFloat(getComputedStyle(cards).gap);
-const cardWidth = cardwidth + gap; 
+const cardWidth = cardwidth + gap;
 
 const updateTransform = () => {
   cards.style.transform = `translateX(${offset}px)`;
@@ -32,27 +31,26 @@ const updateTransform = () => {
 };
 
 const updateArrowState = () => {
-  if (cards.classList.contains('wrap')) {
-    leftArrow.removeEventListener('click', handleLeftArrowClick);
-    rightArrow.removeEventListener('click', handleRightArrowClick);
+  if (cards.classList.contains("wrap")) {
+    leftArrow.removeEventListener("click", handleLeftArrowClick);
+    rightArrow.removeEventListener("click", handleRightArrowClick);
   } else {
-    if (!leftArrow.classList.contains('disabled')) {
-      leftArrow.addEventListener('click', handleLeftArrowClick);
+    if (!leftArrow.classList.contains("disabled")) {
+      leftArrow.addEventListener("click", handleLeftArrowClick);
     }
-    if (!rightArrow.classList.contains('disabled')) {
-      rightArrow.addEventListener('click', handleRightArrowClick);
+    if (!rightArrow.classList.contains("disabled")) {
+      rightArrow.addEventListener("click", handleRightArrowClick);
     }
   }
 };
 
 const handleRightArrowClick = () => {
   offset -= cardWidth;
-  if (offset <= -((cards.children.length) * cardWidth)) {
-    offset = 0; 
+  if (offset <= -(cards.children.length * cardWidth)) {
+    offset = 0;
   }
   updateTransform();
 };
-
 
 const handleLeftArrowClick = () => {
   offset += cardWidth;
@@ -62,11 +60,11 @@ const handleLeftArrowClick = () => {
   updateTransform();
 };
 
-showAllButton.addEventListener('click', () => {
-  cards.classList.toggle('wrap'); 
-  leftArrow.classList.toggle('disabled');
-  rightArrow.classList.toggle('disabled');
-  offset = 0; 
+showAllButton.addEventListener("click", () => {
+  cards.classList.toggle("wrap");
+  leftArrow.classList.toggle("disabled");
+  rightArrow.classList.toggle("disabled");
+  offset = 0;
   updateTransform();
 });
 
